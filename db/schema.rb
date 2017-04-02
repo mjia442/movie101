d0be74a9992ee76f31cc572cb0dc3861cf6cbec5
mjia442 <jmj0707@gmail.com>
2017-04-02 22:28:08 +0800

    user can login/logout/signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170402074629) do
+ActiveRecord::Schema.define(version: 20170402090936) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
