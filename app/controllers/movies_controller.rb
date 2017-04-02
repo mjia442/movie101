cf553a195b49e618fd82bb6c201b233ec81f1c27
mjia442 <jmj0707@gmail.com>
2017-04-02 23:05:53 +0800

    use before_action to find_group_and_check_permission

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @movies = Movie.all
   end
@@ -19,10 +20,10 @@ class MoviesController < ApplicationController
      end
   end
   def edit
-    @movie = Movie.find(params[:id])
+
   end
   def update
-    @movie = Movie.find(params[:id])
+
     if @movie.update(movie_params)
       redirect_to movies_path, notice: "Update Success"
     else
@@ -30,13 +31,20 @@ class MoviesController < ApplicationController
     end
   end
   def destroy
-    @movie = Movie.find(params[:id])
+
     @movie.destroy
     flash[:alert] = "Movie deleted"
     redirect_to movies_path
   end
 
   private
+  def find_movie_and_check_permission
+    @group = Group.find(params[:id])
+
+    if current_user != @movie.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+  end
   def movie_params
   params.require(:movie).permit(:title, :description)
   end
